9a25cea39f6680facc36f05f65da695e9a386572
Sviataslau Apanasionak <s.apanasionak@setplex.com>
2026-06-11 21:50:19 +0300

    fix race conditions in tests

--- core/src/test/java/by/macmonitor/preinittestcontainers/support/ShellFormEntrypointDockerIntegrationTest.java
+++ core/src/test/java/by/macmonitor/preinittestcontainers/support/ShellFormEntrypointDockerIntegrationTest.java
@@ -1,32 +1,25 @@
 package by.macmonitor.preinittestcontainers.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.InspectImageResponse;
 import com.github.dockerjava.api.model.ContainerConfig;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.ContainerLaunchException;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.utility.DockerImageName;
 
 class ShellFormEntrypointDockerIntegrationTest {
 
-    private static final String IMAGE_NAME = "preinit-test/shell-form-time:latest";
-
     @Test
     void runsShellEntrypoint1_printsFormattedTime() {
-        DockerImageName image = DockerImageName.parse(new ImageFromDockerfile(IMAGE_NAME, false)
-                .withFileFromString(
-                        "Dockerfile",
-                        """
+        DockerImageName image = DockerTestImages.build(
+                "preinit-test/shell-form-entrypoint/exec-sh-entrypoint:latest",
+                """
 						FROM alpine:3.21
 						ENTRYPOINT ["/bin/sh", "-c", "date '+%Y-%m-%dT%H:%M:%S'"]
-						""")
-                .get());
+						""");
         try (GenericContainer<?> container = new GenericContainer<>(image)) {
             DockerClient dockerClient = container.getDockerClient();
             InspectImageResponse response =
@@ -44,14 +37,12 @@ class ShellFormEntrypointDockerIntegrationTest {
 
     @Test
     void runsShellEntrypoint2_Error() {
-        DockerImageName image = DockerImageName.parse(new ImageFromDockerfile(IMAGE_NAME, false)
-                .withFileFromString(
-                        "Dockerfile",
-                        """
+        DockerImageName image = DockerTestImages.build(
+                "preinit-test/shell-form-entrypoint/invalid-exec-entrypoint:latest",
+                """
 						FROM alpine:3.21
 						ENTRYPOINT ["date '+%Y-%m-%dT%H:%M:%S'"]
-						""")
-                .get());
+						""");
         try (GenericContainer<?> container = new GenericContainer<>(image)) {
             DockerClient dockerClient = container.getDockerClient();
             InspectImageResponse response =
@@ -60,20 +51,18 @@ class ShellFormEntrypointDockerIntegrationTest {
             assertThat(config.getEntrypoint()).containsExactly("date '+%Y-%m-%dT%H:%M:%S'");
             assertThat(config.getCmd()).isNull();
 
-            assertThatThrownBy(container::start).isInstanceOf(ContainerLaunchException.class);
+            ContainerStartAssertions.assertStartFails(container);
         }
     }
 
     @Test
     void runsShellEntrypoint3_printsFormattedTime() {
-        DockerImageName image = DockerImageName.parse(new ImageFromDockerfile(IMAGE_NAME, false)
-                .withFileFromString(
-                        "Dockerfile",
-                        """
-						FROM alpine:3.21
-						ENTRYPOINT date '+%Y-%m-%dT%H:%M:%S'
-						""")
-                .get());
+        DockerImageName image = DockerTestImages.build(
+                "preinit-test/shell-form-entrypoint/shell-entrypoint:latest",
+                """
+				FROM alpine:3.21
+				ENTRYPOINT date '+%Y-%m-%dT%H:%M:%S'
+				""");
         try (GenericContainer<?> container = new GenericContainer<>(image)) {
             DockerClient dockerClient = container.getDockerClient();
             InspectImageResponse response =
