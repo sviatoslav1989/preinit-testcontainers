687a8306345dd930c557b156658d0017fda216ee
Sviataslau Apanasionak <sviato_slav1989@protonmail.com>
2026-06-15 10:44:08 +0300

    fix compilation errors

--- examples/postgresql/src/test/java/by/macmonitor/examples/postgresql/MavenCentralPostgreSQLPreinitTest.java
+++ examples/postgresql/src/test/java/by/macmonitor/examples/postgresql/MavenCentralPostgreSQLPreinitTest.java
@@ -10,7 +10,7 @@ import com.github.dockerjava.api.DockerClient;
 
 import org.junit.jupiter.api.Test;
 import org.testcontainers.DockerClientFactory;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
